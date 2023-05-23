1d1e3023d451a205ab9ca9b968d267349bc6091e
Doanda37Rahma <ddoanda@gmail.com>
2023-05-23 23:12:16 +0700

    feat: add custom theme

--- app/src/main/java/com/doanda/jetsteam/ui/theme/Color.kt
+++ app/src/main/java/com/doanda/jetsteam/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.doanda.jetsteam.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val SuperDarkBlue = Color(0xFF202126)
+val DarkDullBlue = Color(0xFF1b2838)
+val DarkBlue = Color(0xFF32546d)
+val Blue = Color(0xFF1b9ffa)
--- app/src/main/java/com/doanda/jetsteam/ui/theme/Theme.kt
+++ app/src/main/java/com/doanda/jetsteam/ui/theme/Theme.kt
@@ -5,26 +5,37 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color.Companion.White
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = SuperDarkBlue,
+    primaryVariant = DarkDullBlue,
+    secondary = DarkBlue,
+    secondaryVariant = Blue,
+//    /* Other default colors to override
+    background = DarkDullBlue,
+    surface = DarkBlue,
+    onPrimary = White,
+    onSecondary = White,
+    onBackground = White,
+    onSurface = White,
+//    */
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = SuperDarkBlue,
+    primaryVariant = DarkDullBlue,
+    secondary = DarkBlue,
+    secondaryVariant = Blue,
 
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+//    /* Other default colors to override
+    background = DarkDullBlue,
+    surface = DarkBlue,
+    onPrimary = White,
+    onSecondary = White,
+    onBackground = White,
+    onSurface = White,
+//    */
 )
 
 @Composable
