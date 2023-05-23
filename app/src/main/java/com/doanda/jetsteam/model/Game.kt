6518a0227153860ed1e4b28eb0c50bf5b037a3d7
Doanda37Rahma <ddoanda@gmail.com>
2023-05-23 12:02:19 +0700

    feat: convert raw json to objects

--- app/src/main/java/com/doanda/jetsteam/model/Game.kt
+++ app/src/main/java/com/doanda/jetsteam/model/Game.kt
@@ -1,4 +1,15 @@
 package com.doanda.jetsteam.model
 
-class Game {
-}
+import androidx.room.Entity
+
+@Entity(tableName = "game")
+data class Game(
+    val id: Int,
+    val name: String,
+    val summary: String,
+    val firstReleaseDate: Int,
+    val rating: Any,
+    val cover: String,
+    val genres: List<String>,
+    val platforms: List<String>,
+)
